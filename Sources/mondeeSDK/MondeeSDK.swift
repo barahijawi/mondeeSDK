4ebc65b436dddcac8408c4b8573bc87bf5e4568f
Bara <bbatta@mondee.com>
2023-09-26 14:21:58 -0500

    any

--- Sources/mondeeSDK/MondeeSDK.swift
+++ Sources/mondeeSDK/MondeeSDK.swift
@@ -1,6 +1,8 @@
 import SwiftUI
 
 public struct MondeeView: View {
+    public init() {} // Add a public initializer
+
     public var body: some View {
         Text("Welcome to My Package View!")
     }
