1a8ea230905ef3a4e9ce4cc9817038be546f2947
Bara <bbatta@mondee.com>
2023-09-26 15:02:45 -0500

    any

--- Sources/BrowserViewControllerWrapper.swift
+++ Sources/BrowserViewControllerWrapper.swift
@@ -1,8 +1,14 @@
-//
-//  File.swift
-//  
-//
-//  Created by Bara on 9/26/23.
-//
+import SwiftUI
 
-import Foundation
+
+public struct BrowserViewControllerWrapper: UIViewControllerRepresentable {
+    typealias UIViewControllerType = BrowserViewController
+
+    func makeUIViewController(context: Context) -> BrowserViewController {
+        return BrowserViewController(TabManager())
+    }
+
+    func updateUIViewController(_ uiViewController: BrowserViewController, context: Context) {
+        // Update the view controller if needed
+    }
+}
